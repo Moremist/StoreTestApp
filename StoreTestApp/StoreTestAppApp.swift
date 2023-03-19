251e7a035fa8467eaa1a22495c7fe69414311264
Roman Kuzmich <rbest15@rambler.ru>
2023-03-19 15:56:07 +0300

    Initial Commit

---
+++ StoreTestApp/StoreTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreTestAppApp.swift
+//  StoreTestApp
+//
+//  Created by Roman Kuzmich on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
